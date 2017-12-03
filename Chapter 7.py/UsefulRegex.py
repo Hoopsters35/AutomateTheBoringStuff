e2190788b82f354211420414c0cecda001142f31
Justin Knueppel <justinknueppel@gmail.com>
2017-12-02 21:12:42 -0500

    Add useful methods directory and add compile args to chapter 7

---
+++ Chapter 7.py/UsefulRegex.py
@@ -0,0 +1,20 @@
+import re
+
+#See Python-Workspace/SoloLearnLessons/Regular\ Expressions for more in-depth review
+
+#subl method (word_to_replace, string_to_search) on a compiled regex object
+
+#VERBOSE argument at end of the compile constructor of a regex object allows to compile multi-line regex without counting extra spacing
+#requites triple quotes at start and end of regex
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE)
+#other ending arguments include re.DOTALL which will go past any new line characeters when compiling
+#without re.DOTALL, the regex will only search up until the first \n
+#
+#re.IGNORECASE will ignore case throughout the regex - much more convenient than including both styles
